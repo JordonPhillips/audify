a322e3f603818d124132624d1180d6e658e09a24
JordonPhillips <phjordon@amazon.com>
2019-01-08 11:10:07 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+audify
+======
+
+A simple command line tool to convert a body of text to an mp3 using AWS Polly.
---
+++ audify/__init__.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+import argparse
+import os
+import shutil
+import string
+import sys
+import tempfile
+
+import boto3
+from pydub import AudioSegment
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input-filename', required=True)
+    parser.add_argument('-o', '--output_filename', required=True)
+    parser.add_argument('-v', '--voice', default='Amy')
+    args = parser.parse_args()
+    speechify(
+        input_filename=args.input_filename,
+        output_filename=args.output_filename,
+        voice=args.voice,
+    )
+
+
+def speechify(input_filename, output_filename, voice):
+    tempdir = tempfile.mkdtemp()
+    try:
+        chunk_files = _speechify_chunks(input_filename, voice, tempdir)
+        _stich_chunks(chunk_files, output_filename)
+    finally:
+        shutil.rmtree(tempdir)
+
+
+def _stich_chunks(chunk_files, output_filename):
+    audio = None
+    for chunk_file in chunk_files:
+        segment = AudioSegment.from_mp3(chunk_file)
+        if audio is None:
+            audio = segment
+        else:
+            audio = audio + segment
+
+    if audio:
+        audio.export(output_filename, format='mp3')
+
+
+def _speechify_chunks(filename, voice, tempdir):
+    if filename == '-':
+        return _speechify_fileobj(sys.stdin, voice, tempdir)
+    else:
+        with open(filename) as f:
+            return _speechify_fileobj(f, voice, tempdir)
+
+
+def _speechify_fileobj(fileobj, voice, tempdir):
+    polly = boto3.client('polly')
+    chunk_files = []
+    for i, chunk in enumerate(_read_input(fileobj)):
+        filename = os.path.join(tempdir, '%s.mp3' % i)
+        response = polly.synthesize_speech(
+            OutputFormat='mp3',
+            Text=chunk,
+            VoiceId=voice,
+        )
+        with open(filename, 'wb') as f:
+            f.write(response['AudioStream'].read())
+        chunk_files.append(filename)
+    return chunk_files
+
+
+def _read_input(fileobj, chunk_size=3000):
+    data = fileobj.read(chunk_size)
+
+    while data:
+        text, remainder = _rsplit_whitespace(data)
+        size = chunk_size - len(remainder)
+        yield text
+        data = remainder + fileobj.read(size)
+
+
+def _rsplit_whitespace(input_string):
+    for i in range(len(input_string) - 1, 0, -1):
+        if input_string[i] in string.whitespace:
+            return input_string.rsplit(input_string[i], 1)
+
+
+if __name__ == "__main__":
+    main()
