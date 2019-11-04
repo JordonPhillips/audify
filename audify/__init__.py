2bed4f67644c11f1ac583c61ee167ece6f40f1f4
JordonPhillips <phjordon@amazon.com>
2019-11-04 15:57:02 -0800

    Convert to python 3.8
    This moves the project over to using python 3 features like pathlib,
    type hints (to an extent), and the dreaded walrus operator.

--- audify/__init__.py
+++ audify/__init__.py
@@ -1,41 +1,51 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import argparse
 import os
 import shutil
 import string
 import sys
 import tempfile
+from pathlib import Path
+from typing import List, Iterable, Any
 
 import boto3
 from pydub import AudioSegment
 
+CHUNK_SIZE = 3000
+
 
 def main():
-    parser = argparse.ArgumentParser(description=(
-        'A simple command line tool that uses Amazon Polly to convert text '
-        'files into mp3 files.'
-    ))
-    parser.add_argument(
-        '-i', '--input-filename', required=True,
-        help=(
-            'The file containing the text to convert to speech. If - is '
-            'provided, text will be read from stdin.'
+    parser = argparse.ArgumentParser(
+        description=(
+            "A simple command line tool that uses Amazon Polly to convert text "
+            "files into mp3 files."
         )
     )
     parser.add_argument(
-        '-o', '--output-filename', required=True,
+        "-i",
+        "--input-filename",
+        required=True,
         help=(
-            'The output file name. This file will be in mp3 format.'
-        )
+            "The file containing the text to convert to speech. If - is "
+            "provided, text will be read from stdin."
+        ),
     )
     parser.add_argument(
-        '-v', '--voice', default='Amy',
+        "-o",
+        "--output-filename",
+        required=True,
+        help=("The output file name. This file will be in mp3 format."),
+    )
+    parser.add_argument(
+        "-v",
+        "--voice",
+        default="Amy",
         help=(
-            'The voice to read the text in. Any voice supported by Amazon '
-            'Polly is valid. The default voice is Amy. Other voices can be '
-            'found on the docs for Polly: '
-            'https://docs.aws.amazon.com/polly/latest/dg/voicelist.html'
-        )
+            "The voice to read the text in. Any voice supported by Amazon "
+            "Polly is valid. The default voice is Amy. Other voices can be "
+            "found on the docs for Polly: "
+            "https://docs.aws.amazon.com/polly/latest/dg/voicelist.html"
+        ),
     )
     args = parser.parse_args()
     audify(
@@ -45,7 +55,7 @@ def main():
     )
 
 
-def audify(input_filename, output_filename, voice):
+def audify(input_filename: str, output_filename: str, voice: str):
     """Convert a text file into spoken mp3 file.
 
     :param input_filename: A text file to convert.
@@ -60,14 +70,14 @@ def audify(input_filename, output_filename, voice):
         https://docs.aws.amazon.com/polly/latest/dg/voicelist.html
     :type voice: str
     """
-    if input_filename == '-':
+    if input_filename == "-":
         audify_fileobj(sys.stdin, output_filename, voice)
     else:
-        with open(input_filename) as f:
-            audify_fileobj(f, output_filename, voice)
+        path = Path(input_filename)
+        _audify(Path(input_filename), Path(output_filename), voice)
 
 
-def audify_fileobj(fileobj, output_filename, voice):
+def audify_fileobj(fileobj: Any, output_filename: str, voice: str):
     """Convert a file-like object containing text into spoken mp3 file.
 
     :param fileobj: A file-like object containg text to convert.
@@ -82,15 +92,22 @@ def audify_fileobj(fileobj, output_filename, voice):
         https://docs.aws.amazon.com/polly/latest/dg/voicelist.html
     :type voice: str
     """
-    tempdir = tempfile.mkdtemp()
-    try:
-        chunk_files = _audify_chunks(fileobj, voice, tempdir)
-        _stich_chunks(chunk_files, output_filename)
-    finally:
-        shutil.rmtree(tempdir)
+    with tempfile.TemporaryFile() as f:
+        while data := fileobj.read(CHUNK_SIZE):
+            f.write(data)
+        _audify(Path(f.name), Path(output_filename), voice)
 
 
-def _stich_chunks(chunk_files, output_filename):
+def _audify(
+    input_file: Path, output_file: Path, voice: str, display_progress: bool = False,
+):
+    with tempfile.TemporaryDirectory() as tempdir:
+        temp_path_dir = Path(tempdir)
+        chunk_files = _audify_chunks(input_file, voice, temp_path_dir, display_progress)
+        _stich_chunks(chunk_files, output_file, display_progress)
+
+
+def _stich_chunks(chunk_files: List[Path], output_file: Path, display_progress: bool):
     audio = None
     for chunk_file in chunk_files:
         segment = AudioSegment.from_mp3(chunk_file)
@@ -100,35 +117,36 @@ def _stich_chunks(chunk_files, output_filename):
             audio = audio + segment
 
     if audio:
-        audio.export(output_filename, format='mp3')
+        audio.export(str(output_file), format="mp3")
 
-def _audify_chunks(fileobj, voice, tempdir):
-    polly = boto3.client('polly')
+
+def _audify_chunks(
+    input_file: Path, voice: str, tempdir: Path, display_progress: bool
+) -> List[Path]:
+    polly = boto3.client("polly")
     chunk_files = []
-    for i, chunk in enumerate(_read_input(fileobj)):
-        filename = os.path.join(tempdir, '%s.mp3' % i)
+    for i, chunk in enumerate(_read_input(input_file)):
         response = polly.synthesize_speech(
-            OutputFormat='mp3',
-            Text=chunk,
-            VoiceId=voice,
+            OutputFormat="mp3", Text=chunk, VoiceId=voice
         )
-        with open(filename, 'wb') as f:
-            f.write(response['AudioStream'].read())
-        chunk_files.append(filename)
+        chunk_file = tempdir / ("%s.mp3" % i)
+        chunk_file.write_bytes(response["AudioStream"].read())
+        chunk_files.append(chunk_file)
     return chunk_files
 
 
-def _read_input(fileobj, chunk_size=3000):
-    data = fileobj.read(chunk_size)
+def _read_input(input_file: Path, chunk_size: int = CHUNK_SIZE) -> Iterable[str]:
+    with input_file.open() as fileobj:
+        data = fileobj.read(chunk_size)
 
-    while data:
-        text, remainder = _rsplit_whitespace(data)
-        size = chunk_size - len(remainder)
-        yield text
-        data = remainder + fileobj.read(size)
+        while data:
+            text, remainder = _rsplit_whitespace(data)
+            size = chunk_size - len(remainder)
+            yield text
+            data = remainder + fileobj.read(size)
 
 
-def _rsplit_whitespace(input_string):
+def _rsplit_whitespace(input_string: str):
     for i in range(len(input_string) - 1, 0, -1):
         if input_string[i] in string.whitespace:
             return input_string.rsplit(input_string[i], 1)
--- setup.py
+++ setup.py
@@ -1,38 +1,29 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-with open('README.md') as f:
+with open("README.md") as f:
     README = f.read()
 
 setup(
-    name='audify',
-    version='0.0.3',
-    description=(
-        'A simple command line tool to transform text to speech with AWS.'
-    ),
+    name="audify",
+    version="0.1.0",
+    description=("A simple command line tool to transform text to speech with AWS."),
     long_description=README,
     long_description_content_type="text/markdown",
-    author='Jordon Phillips',
-    author_email='pypi@jkphillips.me',
-    license='MIT',
-    packages=find_packages(exclude=['tests']),
-    url='https://github.com/JordonPhillips/audify',
-    install_requires=[
-        'boto3>=1.9.75<2.0.0',
-        'pydub>=0.23.0,<1.0.0',
-    ],
-    keywords='aws polly tts',
-    entry_points={
-        'console_scripts': [
-            'audify = audify:main'
-        ]
-    },
+    author="Jordon Phillips",
+    author_email="pypi@jkphillips.me",
+    license="MIT",
+    packages=find_packages(exclude=["tests"]),
+    url="https://github.com/JordonPhillips/audify",
+    install_requires=["boto3>=1.9.75<2.0.0", "pydub>=0.23.0,<1.0.0"],
+    python_requires="~=3.8",
+    keywords="aws polly tts",
+    entry_points={"console_scripts": ["audify = audify:main"]},
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-    ]
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.8",
+    ],
 )
